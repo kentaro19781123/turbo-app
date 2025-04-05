c875d052f114ccfc61e1817549444c8fa6713813
kentaro19781123 <kens19781123@gmail.com>
2025-04-05 16:06:13 +0900

    fix: lint error

--- apps/web/tailwind.v1.2.0.config.ts
+++ apps/web/tailwind.v1.2.0.config.ts
@@ -1,6 +1,7 @@
 // tailwind config is required for editor support
 
 import type { Config } from 'tailwindcss'
+
 import sharedConfig from '@repo/ui2/tailwind.config.ts'
 
 const config: Pick<Config, 'presets'> = {
@@ -8,15 +9,3 @@ const config: Pick<Config, 'presets'> = {
 }
 
 export default config
-
-// tailwind config is required for editor support
-
-// import type { Config } from "tailwindcss";
-// import sharedConfig from "@repo/tailwind-config";
-
-// const config: Pick<Config, "content" | "presets"> = {
-//   content: ["./app/**/*.tsx"],
-//   presets: [sharedConfig],
-// };
-
-// export default config;
--- packages/eslint-config/eslint-config.js
+++ packages/eslint-config/eslint-config.js
@@ -5,25 +5,25 @@
  * */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:perfectionist/recommended-natural-legacy",
-    "plugin:unicorn/recommended",
-    "plugin:tailwindcss/recommended",
-    "prettier",
-    "eslint-config-turbo",
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:perfectionist/recommended-natural-legacy',
+    'plugin:unicorn/recommended',
+    'plugin:tailwindcss/recommended',
+    'prettier',
+    'eslint-config-turbo',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
-    "tailwindcss/enforces-negative-arbitrary-values": "off",
-    "tailwindcss/no-custom-classname": [
-      "error",
+    'tailwindcss/enforces-negative-arbitrary-values': 'off',
+    'tailwindcss/no-custom-classname': [
+      'error',
       {
         whitelist: [],
       },
     ],
   },
-  ignorePatterns: [".*.js", "node_modules/", "postcss.config.js"],
-};
+  ignorePatterns: ['.*.js', 'node_modules/', 'postcss.config.js'],
+}
