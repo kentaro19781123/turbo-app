026b688831c84226be1548b35103480f0aa441f6
kentaro19781123 <kens19781123@gmail.com>
2025-08-26 21:29:28 +0900

    feat: 作業3

--- apps/web/src/app/(v2)/test2/page.tsx
+++ apps/web/src/app/(v2)/test2/page.tsx
@@ -3,6 +3,7 @@ import { Card } from '@repo/ui2/card'
 export default function Page() {
   return (
     <div>
+      <p>作業3</p>
       <p className="text-ui2-size md:text-ui2-size">test２</p>
       <Card>UI2 Card</Card>
     </div>
